d1512d8af831e94dc7a46c8f5863bf730d5feb1c
Antoine Drochon <androcho@akamai.com>
2021-02-10 18:39:11 -0800

    Add newline

--- lib/cert.py
+++ lib/cert.py
@@ -145,6 +145,7 @@ class CertificateAPI(BaseAPI):
             idp_api.deploy(idp_id)
         cli.print("Deployment(s) in progress, it typically take 3 to 5 minutes")
         cli.print("Use 'akamai eaa cert crt://%s status' to monitor the progress." % certid)
+
     def delete(self):
         raise NotImplementedError("deletion not implemented")
         pass
