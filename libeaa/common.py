69599be1bf159b596db084dfa70e3236bfacd864
Antoine Drochon <androcho@akamai.com>
2021-04-30 17:26:22 -0700

    bump version to 0.3.4

--- libeaa/common.py
+++ libeaa/common.py
@@ -34,7 +34,7 @@ from akamai.edgegrid import EdgeGridAuth, EdgeRc
 # use the config
 config = EdgeGridConfig({'verbose': False}, 'default')
 
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 
 
 class cli:
