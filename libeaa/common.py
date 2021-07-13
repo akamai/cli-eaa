a60b925148bdcb4f926005f23d9dedcc8e9aebcd
Antoine Drochon <androcho@akamai.com>
2021-07-13 13:24:20 -0700

    Version bump to 0.4.0

--- libeaa/common.py
+++ libeaa/common.py
@@ -35,7 +35,7 @@ from akamai.edgegrid import EdgeGridAuth, EdgeRc
 config = EdgeGridConfig({'verbose': False}, 'default')
 
 #: cli-eaa version
-__version__ = '0.3.9'
+__version__ = '0.4.0'
 
 #: HTTP Request Timeout in seconds
 HTTP_REQ_TIMEOUT = 300
